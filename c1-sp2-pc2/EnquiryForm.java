bec2e48ac93a6696e29caa0dfbf55de41e5d4a4a
katragaddabhavya <134763403+katragaddabhavya@users.noreply.github.com>
2023-05-27 12:52:19 +0530

    Add files via upload

---
+++ c1-sp2-pc2/EnquiryForm.java
@@ -0,0 +1,25 @@
+class EnquiryForm {
+    // Every JAVA program has main method
+    // Program execution begins with the main method
+    public static void main(String[] args) {
+        String name ="john";
+		int phonenumber= 41231255;
+		String email= "john@gmail.com";
+		int age =  34 ;
+		char gender = 'M';
+		String occupation = "Manager";
+		double annualsalary= 10000;
+		String Nationality = "indian";
+		boolean married = true;
+        System.out.println("Name " + name);
+		System.out.println("phone number " +phonenumber);
+		System.out.println("email " +email);
+		System.out.println("age " +age);
+		System.out.println("gender " +gender);
+		System.out.println("occupation " +occupation);
+		System.out.println("annualsalary " +annualsalary);
+		System.out.println("Nationality " +Nationality);
+
+
+    }
+}
---
+++ c1-sp2-pc2/SellingPrice.java
@@ -0,0 +1,12 @@
+class SellingPrice {
+    // Every JAVA program has main method
+    // Program execution begins with the main method
+    public static void main(String[] args) {
+        String itemname ="Kaisa Bowl";
+		float price= 10.99f;
+		float addition =price+ price*0.1f;
+		System.out.println("revised price " +price);
+		
+
+    }
+}
